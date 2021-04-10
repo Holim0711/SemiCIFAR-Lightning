3c486e49c394aa3d25548630845029e2613a09b7
Holim Lim <holim0711@outlook.com>
2021-04-10 21:33:48 +0900

    WIP...

--- semi_cifar/semi_cifar10.py
+++ semi_cifar/semi_cifar10.py
@@ -1,67 +1,74 @@
 import os
+import numpy
 import torch
 import pytorch_lightning as pl
 from torchvision.datasets import CIFAR10
 
 
-class SubsetCIFAR10(CIFAR10):
-    def __init__(self, root, indices=None, **kwargs):
+class SmallCIFAR10(CIFAR10):
+    def __init__(self, root, indices, **kwargs):
         super().__init__(root, **kwargs)
-        if indices is not None:
-            self.data = self.data[indices]
-            self.targets = torch.tensor(self.targets)[indices]
+        assert len(indices) < len(self)
+        self.data = self.data[indices]
+        self.targets = torch.tensor(self.targets)[indices]
 
 
-def index_split(labels, num_labeled):
-    labels = torch.tensor(labels)
+def index_select(labels, n, seed=None):
+    labels = numpy.array(labels)
     classes = labels.unique()
-    labels_per_class = num_labeled // len(classes)
+    n_per_class = n // len(classes)
+    random_state = numpy.random.RandomState(seed)
 
-    idxₗ, idxᵤ = [], []
-
+    indices = []
     for c in classes:
-        indices = torch.where(labels == c)[0]
-        randperm = torch.randperm(len(indices))
-        idxₗ.extend(indices[randperm[:labels_per_class]])
-        idxᵤ.extend(indices[randperm[labels_per_class:]])
-
-    return idxₗ, idxᵤ
+        indices.extend(random_state.choice(
+            numpy.where(labels == c)[0],
+            size=n_per_class,
+            replace=False))
+    return indices
 
 
 class SemiCIFAR10(pl.LightningDataModule):
 
     def __init__(self, root, num_labeled=4000, batch_size=1,
-                 num_workers=None, pin_memory=True):
+                 num_workers=None, pin_memory=True, seed=None, expend=True):
         super().__init__()
         self.root = root
         self.num_labeled = num_labeled
+
         if isinstance(batch_size, dict):
             self.batch_sizeₗ = batch_size['labeled']
             self.batch_sizeᵤ = batch_size['unlabeled']
-        else:
+        elif isinstance(batch_size, int):
             self.batch_sizeₗ = batch_size
             self.batch_sizeᵤ = batch_size
+        else:
+            raise ValueError("batch_size should be 'int' or 'dict'")
+
         self.train_transformₗ = None
         self.train_transformᵤ = None
         self.valid_transform = None
+
         self.num_workers = num_workers if num_workers else os.cpu_count()
         self.pin_memory = pin_memory
+        self.seed = None
+        self.expand = expand
 
     def prepare_data(self):
         CIFAR10(self.root, train=True, download=True)
         CIFAR10(self.root, train=False, download=True)
 
     def setup(self, stage=None):
-        cifar10_train = CIFAR10(self.root, train=True)
+        self.cifar10_valid = CIFAR10(self.root, train=False, transform=self.valid_transform)
+        self.cifar10_trainᵤ = CIFAR10(self.root, train=True, transform=self.train_transformᵤ)
 
-        train_idxₗ, train_idxᵤ = index_split(cifar10_train.targets, self.num_labeled)
+        indices = index_select(self.cifar10_train.targets, self.num_labeled, self.seed)
+        self.cifar10_trainₗ = SmallCIFAR10(self.root, indices, train=True, transform=self.train_transformₗ)
 
-        self.cifar10_trainₗ = SubsetCIFAR10(
-            self.root, train_idxₗ, train=True, transform=self.train_transformₗ)
-        self.cifar10_trainᵤ = SubsetCIFAR10(
-            self.root, train_idxᵤ, train=True, transform=self.train_transformᵤ)
-        self.cifar10_test = CIFAR10(
-            self.root, train=False, transform=self.valid_transform)
+        if self.expand:
+            n_iter = 1 + (len(self.cifar10_trainᵤ) - 1) // self.batch_sizeᵤ
+            indices = indices * (n_iter * self.batch_sizeₗ // len(indices))
+
 
     def train_dataloader(self):
         loaderₗ = torch.utils.data.DataLoader(
@@ -75,7 +82,7 @@ class SemiCIFAR10(pl.LightningDataModule):
     def val_dataloader(self):
         batch_size = max(self.batch_sizeₗ, self.batch_sizeᵤ) * 2
         return torch.utils.data.DataLoader(
-            self.cifar10_test, batch_size, shuffle=False,
+            self.cifar10_valid, batch_size, shuffle=False,
             num_workers=self.num_workers, pin_memory=self.pin_memory)
 
     def test_dataloader(self):
